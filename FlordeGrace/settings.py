fa8692f0c38fc78da0f9c083513bd1477fe6058f
jamiejonescera <152496126+jamiejonescera@users.noreply.github.com>
2024-11-08 14:46:18 +0800

    Adds

--- FlordeGrace/settings.py
+++ FlordeGrace/settings.py
@@ -80,9 +80,6 @@ WSGI_APPLICATION = 'FlordeGrace.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-
-
-
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
